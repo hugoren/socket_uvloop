25a5ad6ba7b9c3918894d61db2d4418a28c300b1
hugo <chenpengren@0easy.com>
2018-02-10 16:57:12 +0800

    ForkingUDPServer比threading速度快

--- service.py
+++ service.py
@@ -1,21 +1,29 @@
 import asyncio
 import uvloop
+import time
 import socketserver
 from config import HOST, PORT
 from utils import log
+from numba import jit
 
 
 class MyUDPHandler(socketserver.BaseRequestHandler):
+
     def handle(self):
         data = self.request[0][:2048]
+        self.batching(data)
+
+    def batching(self, data):
+        start = time.time()
         print(data)
+        print(time.time() - start)
 
 
 async def handler():
     print("Socket udp server begin.....")
     log('info', 'Socket udp server begin.....')
     try:
-        s = socketserver.ThreadingUDPServer((HOST, PORT), MyUDPHandler)
+        s = socketserver.ForkingUDPServer((HOST, PORT), MyUDPHandler)
         s.serve_forever()
     except Exception as e:
         log('error', str(e))
@@ -27,3 +35,5 @@ if __name__ == "__main__":
     loop = uvloop.new_event_loop()
     asyncio.set_event_loop(loop)
     loop.run_until_complete(handler())
+
+    # handler()
